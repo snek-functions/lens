83800d3ea43d2ce25252679d42541da49e82b434
Florian Kleber <kleberbaum@erebos.xyz>
2023-12-18 12:04:05 +0100

    chore: change to local origin

--- src/clients/origin/src/index.ts
+++ src/clients/origin/src/index.ts
@@ -2,5 +2,5 @@ import { makeSnekQuery } from "snek-query";
 import { Query, Mutation } from "./schema.generated.js";
 
 export const sq = makeSnekQuery({ Query, Mutation }, {
-    apiURL: "https://services.snek.at/graphql"
+    apiURL: "http://origin:3000/graphql"
 });
