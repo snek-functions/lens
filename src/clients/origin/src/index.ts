213c3f7a773a4bcfa10a18a010458869470175f9
Florian Kleber <kleberbaum@erebos.xyz>
2023-12-18 14:00:07 +0100

    chore: change to photonq origin

--- src/clients/origin/src/index.ts
+++ src/clients/origin/src/index.ts
@@ -2,5 +2,5 @@ import { makeSnekQuery } from "snek-query";
 import { Query, Mutation } from "./schema.generated.js";
 
 export const sq = makeSnekQuery({ Query, Mutation }, {
-    apiURL: "http://origin:3000/graphql"
+    apiURL: "https://services.photonq.org/graphql"
 });
