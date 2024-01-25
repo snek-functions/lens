ac2b78ff9ceacecf081f3fd7cbe4002ed5b430ea
Nico Schett <nicoschett@icloud.com>
2024-01-25 09:33:09 +0100

    feat(origin): update API URL
    - Updated the API URL in the `index.ts` file to use environment variables or fallback to a default URL.
    - Added a console log statement to indicate the initialization of 'origin-sq' with the updated API URL.

--- src/clients/origin/src/index.ts
+++ src/clients/origin/src/index.ts
@@ -1,6 +1,16 @@
 import { makeSnekQuery } from "snek-query";
 import { Query, Mutation } from "./schema.generated.js";
 
-export const sq = makeSnekQuery({ Query, Mutation }, {
-    apiURL: "https://services.photonq.org/graphql"
-});
+const apiURL =
+  process.env.GATSBY_ORIGIN_API_URL ||
+  process.env.ORIGIN_API_URL ||
+  "https://services.snek.at/graphql";
+
+console.log(`Initialized 'origin-sq' on '${apiURL}'`);
+
+export const sq = makeSnekQuery(
+  { Query, Mutation },
+  {
+    apiURL,
+  }
+);
