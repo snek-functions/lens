65e73d11b2941e18152e0689544b8968d8a044ef
Florian Kleber <kleberbaum@erebos.xyz>
2024-07-15 10:25:48 +0000

    chore: remove permissions adjustmenst

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ chmod 600 -Rv ${HOME}/.ssh/
 
 cp -f /.ansible/..data/.ansible.cfg ${HOME}/
 chmod 600 -Rv ${HOME}/.ansible.cfg
+
 cp -f /.ansible/..data/* ${HOME}/.ansible/
-chmod 600 -Rv ${HOME}/.ansible/
 
 bun pylon-server --https --key /private/tls.key --cert /private/tls.crt --passphrase $PASSPHRASE
